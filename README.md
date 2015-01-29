b9163ebc6f850057adadbfb147fa1d29ed08ff19
Julia Jacobs <jjacobs2000@me.com>
2015-01-29 15:57:53 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Simple Mock Api Server
+
+Just a simple mock api server that serves static JSON files from a folder structure using ExpressJS.
+
+Totally ripped off from [Adam Boczek's blog](https://coderwall.com/p/ss80vw) with some tweaks to use the latest version of ExpressJS.
+
+Thank you Adam Boczek.
+
+If you use this, show him some love by sharing his [blog article](http://twitter.com/share?url=https%3A%2F%2Fcoderwall.com%2Fp%2Fss80vw&via=coderwall&text=Using+Node.js+with+Express+as+a+Simple+API+Mock+Server+%23protip&related=&count=vertical&lang=en)
---
+++ server.js
@@ -0,0 +1,47 @@
+/* Define some initial variables. */
+var applicationRoot = __dirname.replace(/\\/g,"/"),
+  morgan  = require('morgan'),
+  errorhandler = require('errorhandler')
+  ipaddress = '127.0.0.1',
+  port = 8080;
+mockRoot = applicationRoot + '/test/mocks/api',
+  mockFilePattern = '.json',
+  mockRootPattern = mockRoot + '/**/*' + mockFilePattern,
+  apiRoot = '/api',
+  fs = require("fs"),
+  glob = require("glob");
+
+/* Create Express application */
+var express = require("express");
+var app = express();
+
+/* Configure a simple logger and an error handler. */
+app.use(morgan('combined'));
+app.use(errorhandler());
+
+/* Read the directory tree according to the pattern specified above. */
+var files = glob.sync(mockRootPattern);
+
+/* Register mappings for each file found in the directory tree. */
+if(files && files.length > 0) {
+  files.forEach(function(fileName) {
+
+    var mapping = apiRoot + fileName.replace(mockRoot, '').replace(mockFilePattern,'');
+
+    app.get(mapping, function (req, res) {
+      var data =  fs.readFileSync(fileName, 'utf8');
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.write(data);
+      res.end();
+    });
+
+    console.log('Registered mapping: %s -> %s', mapping, fileName);
+  })
+} else {
+  console.log('No mappings found! Please check the configuration.');
+}
+
+/* Start the API mock server. */
+console.log('Application root directory: [' + applicationRoot +']');
+console.log('Mock Api Server listening: [http://' + ipaddress + ':' + port + ']');
+app.listen(port, ipaddress);
